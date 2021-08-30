756a95d3acabe7ed2aa91d97c19a4018e9b6eba6
simondlevy <simon.d.levy@gmail.com>
2021-08-30 13:54:08 -0400

    Replicated Ross's first two plots

--- python/test1.py
+++ python/test1.py
@@ -5,31 +5,34 @@ import vsa
 import numpy as np
 import matplotlib.pyplot as plt
 
-# make an encoder specification with realistic vector dimension
-ss = vsa.mk_scalar_encoder_spline_spec(1000, (-1, 1, 2, 4))
+for dims in 1000,10000:
 
-# get the vectors corresponding to the knots
-vecs = ss['knots_vsa']
-v1 = vecs[0]
-v2 = vecs[1]
-v3 = vecs[2]
-v4 = vecs[3]
+    # make an encoder specification with realistic vector dimension
+    ss = vsa.mk_scalar_encoder_spline_spec(dims, (-1, 1, 2, 4))
 
-# make a sequence of scalar values that (more than) span the knot range
-x = np.arange(-1.5, 4.5, .05)
+    # get the vectors corresponding to the knots
+    vecs = ss['knots_vsa']
+    v1 = vecs[0]
+    v2 = vecs[1]
+    v3 = vecs[2]
+    v4 = vecs[3]
 
-# get the cosine between the encoded x and each of the knot vectors
-cos_1 = [vsa.cos_sim(vsa.encode_scalar_spline(xval, ss), v1) for xval in x]
-cos_2 = [vsa.cos_sim(vsa.encode_scalar_spline(xval, ss), v2) for xval in x]
-cos_3 = [vsa.cos_sim(vsa.encode_scalar_spline(xval, ss), v3) for xval in x]
-cos_4 = [vsa.cos_sim(vsa.encode_scalar_spline(xval, ss), v4) for xval in x]
+    # make a sequence of scalar values that (more than) span the knot range
+    x = np.arange(-1.5, 4.5, .05)
 
-# plot the cosines
-plt.plot(cos_1, '.')
-plt.plot(cos_2, '.')
-plt.plot(cos_3, '.')
-plt.plot(cos_4, '.')
-plt.xlabel('x')
-plt.ylabel('cos')
-plt.legend(['knot1', 'knot2', 'knot3', 'knot4'])
-plt.show()
+    # get the cosine between the encoded x and each of the knot vectors
+    cos_1 = [vsa.cos_sim(vsa.encode_scalar_spline(xval, ss), v1) for xval in x]
+    cos_2 = [vsa.cos_sim(vsa.encode_scalar_spline(xval, ss), v2) for xval in x]
+    cos_3 = [vsa.cos_sim(vsa.encode_scalar_spline(xval, ss), v3) for xval in x]
+    cos_4 = [vsa.cos_sim(vsa.encode_scalar_spline(xval, ss), v4) for xval in x]
+
+    # plot the cosines
+    plt.plot(cos_1, '.')
+    plt.plot(cos_2, '.')
+    plt.plot(cos_3, '.')
+    plt.plot(cos_4, '.')
+    plt.xlabel('x')
+    plt.ylabel('cos')
+    plt.legend(['knot1', 'knot2', 'knot3', 'knot4'])
+    plt.title('Dims = %d' % dims)
+    plt.show()

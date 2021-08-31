940afd54bd6e215d4ed63f1354b1363e4b040bfc
simondlevy <simon.d.levy@gmail.com>
2021-08-31 17:08:53 -0400

    =More Python migration

--- python/test3.py
+++ python/test3.py
@@ -6,19 +6,21 @@ Replicates section 3.1 of
 
 import vsa
 
-def report(expected, actual):
-    print('Expect: %f; got %f' % (expected, actual))
-
-# make an encoder specification with realistic vector dimension
-ss = vsa.mk_scalar_encoder_spline_spec(10000, (-1, 1, 2, 4))
-
-for expected, encoded in ((-1, -1.5), (-1, -1), (0, 0), (1, 1), (1.5, 1.5)):
+def test(expected, encoded):
 
     obtained = vsa.decode_scalar_spline(vsa.encode_scalar_spline(encoded, ss), ss)
 
     print('Expect: %f; got %f' % (expected, obtained))
 
+# Make an encoder specification with realistic vector dimension
+ss = vsa.mk_scalar_encoder_spline_spec(10000, (-1, 1, 2, 4))
 
-# The decoded values at the knots are exactly correct.
-# The decoded values between the knots are approximately correct.
-# Check the random variation of intermediate values.
+# Check that encoded values are decoded correctly across the range of the knots.
+for expected, encoded in ((-1, -1.5), (-1, -1), (0, 0), (1, 1), (1.5, 1.5)):
+    test(expected, encoded)
+
+print()
+
+# Check the random variation of intermediate values
+for _ in range(5):
+    test(0, 0)

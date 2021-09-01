d86a7c32c01c4cc856e3c3d656439c61899211d6
simondlevy <simon.d.levy@gmail.com>
2021-09-01 17:18:52 -0400

    =More test4 work

--- python/test4.py
+++ python/test4.py
@@ -6,18 +6,23 @@ Replicates third pair of figures from
 
 import vsa
 import numpy as np
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 
 ss = vsa.mk_scalar_encoder_spline_spec(10000, np.arange(100))
 
-x_in = [x for x in np.linspace(-0.1, +1.1, 1000)]
+x_in = [x for x in np.linspace(-0.1, +1.1, 100)]
 
 zero_thresh = 8
 
 x_out = [vsa.decode_scalar_spline(
     vsa.encode_scalar_spline(x, ss), ss, zero_thresh) for x in x_in]
 
-plt.scatter(x_out, x_in, s=0.1)
+r = stats.linregress(x_in, x_out)
+
+plt.scatter(x_in, x_out, s=0.1)
+plt.xlim([-0.1, +1.1])
+#plt.plot(x_in, r.intercept + r.slope*x_in, 'r')
 plt.xlabel('x_in')
 plt.ylabel('x_out')
 plt.title('zero_thresh = %d' % zero_thresh)
